87d3bd8ab2e8190c4466fc15ec2b41906d6ecbe2
lukedukeus <vanzweden1@gmail.com>
2025-06-04 16:33:37 -0400

    new problem

--- MartenBug/Program.cs
+++ MartenBug/Program.cs
@@ -1,14 +1,12 @@
 ﻿using JasperFx;
 using JasperFx.Events;
-
+using JasperFx.Events.Projections;
 using Marten;
-
+using Marten.Events.Aggregation;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 using NetTopologySuite.Utilities;
-
-using Spectre.Console;
 
 using Testcontainers.PostgreSql;
 
@@ -31,100 +29,88 @@ namespace MartenBug
                     options.AutoCreateSchemaObjects = AutoCreate.All;
 
                     options.Events.AddEventType<UserCreated>();
+
+                    options.Schema.For<EventSummary>();
+                    options.Projections.Add<EventSummaryProjection>(ProjectionLifecycle.Inline);
                 });
             })
            .Build();
 
             IQuerySession querySession = host.Services.GetRequiredService<IQuerySession>();
             IDocumentStore documentStore = host.Services.GetRequiredService<IDocumentStore>();
+            IServiceProvider serviceProvider = host.Services.GetRequiredService<IServiceProvider>();
 
-            int testEvents = 10;
             var userId = Guid.CreateVersion7();
-            List<IDatabaseEvent> userEvents = new List<IDatabaseEvent>();
+            UserReference user = new UserReference(userId, "testuser", "secret");
 
             await using (var session = documentStore.LightweightSession())
             {
-                var username = $"User";
-                var password = "super secret";
+                var userCreatedEvent = new UserCreated(user);
 
-                var userCreatedEvent = new UserCreated(userId, username, password);
-                userEvents.Add(userCreatedEvent);
-
-                var stream = session.Events.StartStream(userCreatedEvent);
+                var stream = session.Events.StartStream<ICreatedEvent>(userCreatedEvent.StreamId, userCreatedEvent);
 
                 await session.SaveChangesAsync();
+            }
 
-                userId = stream.Id;
+            await using (var session = documentStore.LightweightSession())
+            {
+                var userLoggedInEvent = new UserLoggedIn(user);
 
-                for (int i = 1; i < testEvents; i++)
-                {
-                    var userUpdateEvent = new UserUpdated(userId, $"User {i}");
-                    userEvents.Add(userUpdateEvent);
+                var stream = session.Events.Append(userLoggedInEvent.StreamId, 2, userLoggedInEvent);
 
-                    session.Events.Append(userId, userUpdateEvent);
-                    await session.SaveChangesAsync();
-                }
+                await session.SaveChangesAsync();
             }
 
             // Works fine
-            var userEventsFromListBackedIQueryable = userEvents
-                .Select(x => new ClassWithPrimaryConstructor(x))
-                .ToList();
+            var eventSummaries = querySession.Query<EventSummary>().ToList();
 
-            // Works fine
-            var userEventsByRequiredProps = await querySession.Events.QueryAllRawEvents()
-                .Where(x => x.StreamId == userId)
-                .OrderBy(x => x.Version)
-                .Select(x => new AuditEventWithRequiredProps()
-                {
-                    EventId = x.Id,
-                    RecordVersion = x.Version,
-                    TimestampUtc = x.Timestamp,
-                    Name = "TODO",
-                    Description = "TODO"
-                })
-                .ToListAsync();
-
-            // Throws System.InvalidOperationException: 'Sequence contains no elements'
-            var userEventsByPrimaryConstructor = await querySession.Events.QueryAllRawEvents()
-                .Where(x => x.StreamId == userId)
-                .OrderBy(x => x.Version)
-                .Select(x => new AuditEventWithPrimaryConstructor(x))
-                .ToListAsync();
-
-            Assert.Equals(testEvents, userEventsFromListBackedIQueryable.Count);
-            Assert.Equals(testEvents, userEventsByPrimaryConstructor.Count);
-            Assert.Equals(testEvents, userEventsByRequiredProps.Count);
+            Assert.Equals(eventSummaries.Count, 2);
         }
     }
 
-    public record UserCreated(Guid Id, string Username, string Password) : IDatabaseEvent;
-    public record UserUpdated(Guid Id, string Username) : IDatabaseEvent;
+    public record UserReference(Guid Id, string Username, string Password) : RecordReference(Id);
+    public record RecordReference(Guid Id);
+
+    public record UserCreated(UserReference user) : ICreatedEvent
+    {
+        public Guid StreamId => user.Id;
+    }
+
+    public record UserLoggedIn(UserReference user) : IDatabaseEvent
+    {
+        public Guid StreamId => user.Id;
+    }
+
+    public interface ICreatedEvent : IDatabaseEvent
+    {
+
+    }
+
     public interface IDatabaseEvent
     {
-        Guid Id { get; }
+        Guid StreamId { get; }
     }
 
-    public class AuditEventWithPrimaryConstructor(IEvent @event)
+    public class EventSummary
     {
-        public Guid EventId { get; set; } = @event.Id;
-        public long RecordVersion { get; set; } = @event.Version;
-        public DateTimeOffset TimestampUtc { get; set; } = @event.Timestamp;
-        public string Name { get; set; } = "TODO";
-        public string Description { get; set; } = "TODO";
-    }
-
-    public class AuditEventWithRequiredProps
-    {
-        public required Guid EventId { get; set; }
-        public required long RecordVersion { get; set; }
+        public required Guid Id { get; set; }
+        public required Guid EventTypeId { get; set; }
         public required DateTimeOffset TimestampUtc { get; set; }
-        public required string Name { get; set; } = "TODO";
-        public required string Description { get; set; } = "TODO";
+        public required IEnumerable<RecordReference> Records { get; set; }
     }
 
-    public class ClassWithPrimaryConstructor(IDatabaseEvent @event)
+    public class EventSummaryProjection : SingleStreamProjection<EventSummary, Guid>
     {
-
+        public static EventSummary Create(IEvent<IDatabaseEvent> @event)
+        {
+            return new EventSummary()
+            {
+                Id = Guid.CreateVersion7(),
+                EventTypeId = @event.Data.GetType().GUID,
+                TimestampUtc = @event.Timestamp,
+                Records = []
+                //Records = @event.Data.EventDescription.Description.Where(x => x.IsRecordReference).Select(x => x.AsRecordReference()),
+            };
+        }
     }
 }
